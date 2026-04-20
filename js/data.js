4b0ec9b53d1001e9c6fff708f27d413421e717eb
Aclarosb <aclarosb@Unbosque.edu.co>
2026-04-20 20:04:53 +0000

    data

--- js/data.js
+++ js/data.js
@@ -1,96 +1,91 @@
 let productos = [
  {
     id: 1,
-    nombre: "curry 9",
-    descripcion: "zapatillas curry 9 rojas",
-    precio: 32000,                            
-    imagen: "https://i.postimg.cc/QCKbLJbT/curry_9_rojas.webp",
-   descuento:0.15,
-   enStock:true
+    nombre: "Balón de fútbol profesional talla 5",
+    descripcion: "Balón resistente de alta calidad, ideal para partidos y entrenamientos en césped o cancha sintética.",
+    precio: 45000,                            
+    imagen: "https://images.unsplash.com/photo-1660926655155-8b1f8f9079f5?fm=jpg&q=60&w=3000&auto=format&fit=crop&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxzZWFyY2h8M3x8YmFsJUMzJUIzbiUyMGRlJTIwZiVDMyVCQXRib2x8ZW58MHx8MHx8fDA%3D",
+    descuento: 0.15,
+    enStock: true
   },
 
   {
     id: 2,
-    nombre: "curry 9 mostruo come galletas",
-    descripcion: "zapatillas curry 9 edicion mostruo come galletas",
-    precio: 58000,
-    imagen: "https://i.postimg.cc/yx3PqyP3/curry_9_comegalletas.jpg",
-   descuento:0.15,
-   enStock:true
+    nombre: "Zapatillas de running ligeras para hombre",
+    descripcion: "Tenis deportivos cómodos y transpirables, ideales para correr y entrenamientos diarios.",
+    precio: 120000,
+    imagen: "https://contents.mediadecathlon.com/p2976616/k$0759ff7dafd21402309d80f41ed12b35/tenis-running-adidas-galaxy-7-hombre.jpg",
+    descuento: 0.15,
+    enStock: true
   },
  
   {
     id: 3,
-    nombre: "balon curry",
-    descripcion: "balon de caucho marca curry ",
-    precio: 24000,
-    imagen: "https://i.postimg.cc/vTxzRtzc/balon_curry.jpg",
-   descuento:0.15,
-   enStock:true
+    nombre: "Guantes de gimnasio antideslizantes",
+    descripcion: "Guantes ergonómicos con agarre reforzado para levantamiento de pesas y entrenamiento funcional.",
+    precio: 35000,
+    imagen: "https://fabysport.com/wp-content/uploads/2023/03/guante-get-fitness-damagf60dmA-2.webp",
+    descuento: 0.15,
+    enStock: true
   },
   
   {
     id: 4,
-    nombre: "sudadera curry brand",
-    descripcion: "sudadera transpirable marca curry",
-    precio: 19500,
-    imagen: "https://i.postimg.cc/qq6LfxLg/sudadera_under_armour_curry_splash_graphic_black_hyper_green_1.webp",
-   descuento:0.15,
-   enStock:true
+    nombre: "Mancuernas ajustables de 10 kg",
+    descripcion: "Set de mancuernas ajustables perfecto para entrenar fuerza en casa con diferentes niveles de peso.",
+    precio: 90000,
+    imagen: "https://sportfitness.co/cdn/shop/files/0c24531e-0f75-4000-aa4f-1e9acb5d2e9b.jpg?v=1769183309",
+    descuento: 0.15,
+    enStock: true
   },
 
   {
     id: 5,
-    nombre: "uniforme gs",
-    descripcion: "uniforme de golden state warriors",
-    precio: 15000,
-    imagen: "https://i.postimg.cc/1XVHxrHX/uniforme_gs.jpg",
-   descuento:0.45,
-   enStock:true
+    nombre: "Cuerda para saltar de alta velocidad",
+    descripcion: "Cuerda ligera y resistente, ideal para cardio, crossfit y entrenamiento de agilidad.",
+    precio: 20000,
+    imagen: "https://contents.mediadecathlon.com/p2568401/k$095d4e977e83998ef2ffcaf50953b94b/cuerda-para-saltar-jr500-goma.jpg",
+    descuento: 0.45,
+    enStock: true
   },
 
   {
     id: 6,
-    nombre: "rodilleras",
-    descripcion: "rodilleras para baloncesto",
-    precio: 27000,
-    imagen: "https://i.postimg.cc/brSTX9Ty/rodilleras.webp",
-   descuento:0.15,
-   enStock:true
-  }
-  ,
+    nombre: "Bolso deportivo multifuncional",
+    descripcion: "Bolso amplio y resistente con múltiples compartimentos para llevar ropa y accesorios deportivos.",
+    precio: 60000,
+    imagen: "https://fuegoverde.com.co/wp-content/uploads/2019/03/T_60_PRINCIPAL.jpg",
+    descuento: 0.15,
+    enStock: true
+  },
 
   {
     id: 7,
-    nombre: "curry 4 flowtro",
-    descripcion: "zapatillas curry 4 edicion flowtro finales 2022",
-    precio: 22000,
-    imagen: "https://i.postimg.cc/BbPgWBg8/curry_4_flowtro.avif",
-   descuento:0.15,
-   enStock:true
-  }
-
-  ,
+    nombre: "Botella térmica deportiva 750 ml",
+    descripcion: "Botella reutilizable que mantiene la temperatura de tus bebidas frías o calientes por horas.",
+    precio: 30000,
+    imagen: "https://www.sportline.com.co/media/catalog/product/z/8/z80145_frontf1-001.jpg?optimize=medium&bg-color=255,255,255&fit=bounds&height=&width=&canvas=:",
+    descuento: 0.15,
+    enStock: true
+  },
 
   {
     id: 8,
-    nombre: "curry 12",
-    descripcion: "zapatillas curry 12",
-    precio: 47000,
-    imagen: "https://i.postimg.cc/8cfH8mHr/curry_12.webp",
-   descuento:0.15,
-   enStock:true
-  }
-
-  ,
+    nombre: "Colchoneta de yoga antideslizante",
+    descripcion: "Mat cómodo y resistente, ideal para yoga, pilates y ejercicios en casa.",
+    precio: 55000,
+    imagen: "https://contents.mediadecathlon.com/p2940174/k$653682a24ccebece5b9f9cc1f8049d61/colchoneta-yoga-grip-185cm-x-65cm-x-4mm-azul.jpg",
+    descuento: 0.15,
+    enStock: true
+  },
 
   {
     id: 9,
-    nombre: "curry 1",
-    descripcion: "zapatillas curry 1",
-    precio: 67000,
+    nombre: "Zapatos de senderismo resistentes",
+    descripcion: "Calzado cómodo y duradero, diseñado para caminatas en montaña y terrenos irregulares.",
+    precio: 140000,
     imagen: "https://i.postimg.cc/QCKbLJbM/curry_1.webp",
-   descuento:0.15,
-   enStock:true
+    descuento: 0.15,
+    enStock: true
   }
-  ]
+]
